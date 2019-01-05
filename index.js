5ef52f7cfe79daf30edd9fd702ab782ef1d6f060
Hunt Collings <hunt@collings.co.uk>
2019-01-05 11:23:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,67 @@
-document.addEventListener("DOMContentloaded", (e) => {
-  // invoking a function here will make sure it happens on page load
-  // your code here
+
+const button = document.getElementById('Titanic');
+
+button.addEventListener('click', function() {
+  populateTitanic();
+})
+
+
+ 
+ function populateTitanic () {
+  
+
+  const title =  movies.Titanic.title;
+  
+  document.getElementById('title').innerHTML = title;
+  
+  const director = movies.Titanic.director
+  
+document.getElementById("director").innerHTML = director;
+ 
+ const genre = movies.Titanic.genre;
+ 
+ document.getElementById("genre").innerHTML = genre;
+ 
+ const filmRating = movies.Titanic.filmRating;
+  
+document.getElementById("filmRating").innerHTML = filmRating;
+
+ const movieDescription = movies.Titanic.description;
+  
+document.getElementById("description").innerHTML = movieDescription;
+
+const score = movies.Titanic.audienceScore;
+
+document.getElementById("audienceScore").innerHTML = score;
+
+
+function populateCast(){  
+
+var a = [];
+movies.Titanic.cast.forEach(function(obj){
+a.push(obj.role,obj.actor);
+
+
 });
+
+document.getElementById("cast").innerHTML = a
+
+
+}
+
+populateCast();
+}
+
+
+
+
+
+
+
+
+//document.addEventListener("DOMContentloaded", (e) => {
+//  });
+
+
+
+
